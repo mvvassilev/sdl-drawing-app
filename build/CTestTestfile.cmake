e7a068c73138cf91bf2b9b3a7f34bd6f8af616e7
mvvassilev <m.v.vassilev@abv.bg>
2022-07-08 15:18:34 +0300

    Add /build dir

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Documents/Freelancer/sdl-drawing-app
+# Build directory: /home/user/Documents/Freelancer/sdl-drawing-app/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
